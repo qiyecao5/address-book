906d137ef52c7ad78226bad83af1d7c38cfd9f80
七叶草 <609087282@qq.com>
2023-10-23 11:51:47 +0800

    aaaaa

--- 通讯录管理系统/通讯录管理系统.cpp
+++ 通讯录管理系统/通讯录管理系统.cpp
@@ -18,8 +18,35 @@ void showMenu() {
  
 
 int main() {
+	int select = 0;  //创建用户选择输入的变量
+	
 	// 菜单调用.
 	showMenu();
+	cin >> select;
+
+	switch (select)
+	{
+	case 1:		//1、添加联系人
+		break;
+	case 2:		//2、显示联系人
+		break;
+	case 3:		//3、删除联系人
+		break;
+	case 4:		//4、查找联系人
+		break;
+	case 5:		//5、修改联系人
+		break;
+	case 6:		//6、清空联系人
+		break;
+	case 0:		//0、退出通讯录
+		cout << "欢迎下次使用" << endl;
+		system("pause");
+		return 0;
+		break;
+	default:
+		break;
+	}
+
 
 	system("pause");
 	return 0;
