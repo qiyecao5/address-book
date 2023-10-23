ddf1f048236022b821599bb0cc3b81140bb9ef12
七叶草 <609087282@qq.com>
2023-10-24 00:09:54 +0800

    修改指定联系人信息

--- 通讯录管理系统/通讯录管理系统.cpp
+++ 通讯录管理系统/通讯录管理系统.cpp
@@ -59,7 +59,6 @@ void addPerson(Addressbooks* abs) {
 		cout << "请输入性别：" << endl;
 		cout << "1 --- 男" << endl;
 		cout << "2 --- 女" << endl;
-		cout << "3 --- 保密" << endl;
 		int sex = 0;
 		while (true)
 		{	//	如果输入的是1或2 退出循环，因为输入正确
@@ -203,6 +202,63 @@ void findPerson(Addressbooks * abs) {
 	system("cls");
 } 
 
+//5.修改指定联系人信息
+void modifyPerson(Addressbooks* abs) {
+	cout << "请输入您要修改的联系人" << endl;
+	string name;
+	cin >> name;
+	//判断指定的联系人是否存在通讯录中
+	int ret = isExist(abs, name);
+	if (ret != -1) //找到指定联系人
+	{
+		//姓名
+		string name;
+		cout << "请输入姓名：" << endl;
+		cin >> name;
+		abs->personArray[ret].m_Name = name;
+		//性别
+		cout << "请输入性别：" << endl;
+		cout << "1 --- 男" << endl;
+		cout << "2 --- 女" << endl;
+		int sex = 0;
+		while (true)
+		{	//	如果输入的是1或2 退出循环，因为输入正确
+			// 如果输入的有误 就重新输入
+			cin >> sex;
+			if (sex == 1 || sex == 2)
+			{
+				abs->personArray[ret].m_Sex = sex;
+				break;
+			}
+			else
+			{
+				cout << "你输入的不正确" << endl;
+			}
+		}
+		//年龄
+		cout << "请输入年龄：" << endl;
+		int age = 0;
+		cin >> age;
+		abs->personArray[ret].m_Age = age;
+		//电话
+		cout << "请输入联系电话：" << endl;
+		string phone;
+		cin >> phone;
+		abs->personArray[ret].m_Phone = phone;
+		//住址
+		cout << "请输入家庭地址：" << endl;
+		string address;
+		cin >> address;
+		abs->personArray[ret].m_Addr = address;
+		cout << "修改成功" << endl;
+	}
+	else 
+	{
+		cout << "查无此人" << endl;
+	}
+	system("pause");
+	system("cls");
+}
 
 //20.05
 int main() {
@@ -210,9 +266,7 @@ int main() {
 	Addressbooks abs;
 	//初始化通讯录中当前人员个数
 	abs.m_Size = 0;
-
 	int select = 0;  //创建用户选择输入的变量
-
 
 	while (true)
 	{
@@ -228,24 +282,13 @@ int main() {
 			showPerson(&abs);
 			break;
 		case 3:		//3、删除联系人
-		//{//如果你用switch语句，判断中的语句很复杂，你要把这片都包起来，否则报错
-		//	cout << "请输入删除联系人姓名：" << endl;
-		//	string name;
-		//	cin >> name;
-		//	if (isExist(&abs, name) == -1) {
-		//		cout << "查无此人" << endl;
-		//	}
-		//	else
-		//	{
-		//		cout << "找到此人" << endl;
-		//	}
-		//}
 			deletePerson(&abs);
 			break;
 		case 4:		//4、查找联系人
 			findPerson(&abs);
 			break;
 		case 5:		//5、修改联系人
+			modifyPerson(&abs);
 			break;
 		case 6:		//6、清空联系人
 			break;
